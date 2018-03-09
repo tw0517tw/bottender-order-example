d2ab7f36f40b1cd89ba2ab4fbe7fb1beb45ea021
tw0517tw <tw0517tw@gmail.com>
2018-03-09 16:02:06 +0800

    move handler apart

---
+++ handlers/index.js
@@ -0,0 +1,3 @@
+module.exports = async context => {
+  await context.sendText('Hello World');
+};
--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const { LineBot } = require('bottender');
 const { createServer } = require('bottender/express');
 
 const config = require('./bottender.config.js').line;
+const handler = require('./handlers/');
 
 const bot = new LineBot({
   accessToken: config.accessToken,
   channelSecret: config.channelSecret,
 });
 
-bot.onEvent(async context => {
-  await context.sendText('Hello World');
-});
+bot.onEvent(handler);
 
 const server = createServer(bot);
 
